720dcc6c72b4c9aa9807c7218e4fee8bd3bb9dba
Robert Vásquez <vasquez.robert.andres@gmail.com>
2021-07-27 13:14:02 -0400

    Iniciando proyecto, clase doctor creada

---
+++ Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor{
+  //Atributos
+  int id;
+  String name;
+  String speciality;
+
+  //Comportamientos o Métodos
+  public void showName(){
+    System.out.println(name);
+  }
+  
+}
